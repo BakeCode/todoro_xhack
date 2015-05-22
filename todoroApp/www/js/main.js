b750a81b8b683ab6fb6612e3436ddbec4c4821ad
Lioman <dev@lioman.de>
2015-05-22 18:45:38 +0200

    add js to main.js

--- todoroApp/www/js/main.js
+++ todoroApp/www/js/main.js
@@ -2,3 +2,22 @@
  * Created by denis on 22.05.15.
  */
 
+$(document).foundation();
+var i;
+function addTask() {
+    text = $('#taskText').val();
+    if (text != '') {
+        $('#tasks').append(
+            '<li><a href="#" class="button" id="' + i +'">' + text + '</a></li >');
+        i++;
+        $('#taskText').val(" ");
+    }
+    else
+        alert("Are you stupid?")
+
+}
+function openTask() {
+    $('#page1').hide();
+    $('#page2').show();
+    $('#page2').html($('#i').val());
+}
