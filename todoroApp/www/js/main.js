8aa267a03ab64cc100a9a93fb0a4a8842884ba30
Lioman <dev@lioman.de>
2015-05-25 00:36:00 +0200

    status at the end of xhacks

--- todoroApp/www/js/main.js
+++ todoroApp/www/js/main.js
@@ -2,38 +2,39 @@ $(document).foundation();
 var i;
 var GlobalWriter;
 
+
 $(document).ready(function(){
-
-    localStorage.setItem('0',"cooking");
-    localStorage.setItem('1',"learn python");
-    localStorage.setItem('2',"read a book");
-    localStorage.setItem('3',"do s.th");
-    localStorage.setItem('4',"do anything else");
-    i = 5;
-
-    for (var i = 0; i < localStorage.length; i++){
-        $('#tasks').append(
-            '<li><a href="#" class="button" id="' + i + '" onclick="openTask(event);">' + localStorage.getItem(localStorage.key(i)) + '</a></li >'
-
-        );
-    }
+    localStorage.setItem('counter', 0);
+    //localStorage.setItem('0',"cooking");
+    //localStorage.setItem('1',"learn python");
+    //localStorage.setItem('2',"read a book");
+    //localStorage.setItem('3',"do s.th");
+    //localStorage.setItem('4',"do anything else");
+    //i = 5;
+    //
+    //for (var i = 0; i < localStorage.length; i++){
+    //    $('#tasks').append(
+    //        '<li><a href="#" class="button" id="' + i + '" onclick="openTask(event);">' + localStorage.getItem(localStorage.key(i)) + '</a></li >'
+    //
+    //    );
+    //}
+    parseFile();
     onDeviceReady();
-
-
-
-
 
 });
 
 function parseFile() {
     $.get('todo.txt').done(function (data) {
+    //$.get('https://raw.githubusercontent.com/BakeCode/todoro/master/todoroApp/www/todo.txt').done(function (data) {
         todo = TodoTxt.parseFile(data);
         list = todo.items.call();
-
-
+ var _class='primary';
         for (task of list) {
+            if (task.isComplete()){
+                _class ='secondary';
+            }
             $('#tasks').append(
-                '<li><a href="#" class="button" id="' + i + '" onclick="openTask(event);">' + task.textTokens().join(' ') + '</a></li >');
+                '<li><a href="#" class="button " + _class + " id="' + i + '" onclick="openTask(event);">' + task.textTokens().join(' ') + '</a></li >');
         }
 
 
@@ -129,6 +130,10 @@ function secondPassed() {
         progressBar.removeClass('primary').addClass('success');
     }
     if (seconds == 0) {
+        var count= localStorage.getItem('counter');
+        count++;
+        localStorage.setItem('counter', count);
+        $('#counter').html(count);
         clearInterval(countdownTimer);
         $('#startbutton').html("00:00");
     } else {
